82764aefa24699f4a0885f65d74281009028cb03
c_ringwald <42188996+datalogism@users.noreply.github.com>
2022-09-19 13:31:30 +0200

    Update computeStatsLang.sh

--- dbpedia-loader/process/computeStatsLang.sh
+++ dbpedia-loader/process/computeStatsLang.sh
@@ -9,8 +9,8 @@ mkdir dbpedia_dumps_lang_fr_related
 rm -f ./dbpedia_dumps_lang_fr_related/*
 
 ### MAYBE CHANGE IT FROM FILE USE IN LOADING
-query=$(curl -H "Accept:text/sparql" https://databus.dbpedia.org/cringwald/collections/dbpediabylang)
-files=$(curl -H "Accept: text/csv" --data-urlencode "query=${query}" https://databus.dbpedia.org/repo/sparql | tail -n+2 | sed 's/"//g')
+query=$(curl -k -H "Accept:text/sparql" https://databus.dbpedia.org/cringwald/collections/dbpediabylang)
+files=$(curl -k -H "Accept: text/csv" --data-urlencode "query=${query}" https://databus.dbpedia.org/repo/sparql | tail -n+2 | sed 's/"//g')
 while IFS= read -r file ; do wget -P ./dbpedia_dumps_lang_fr_related $file; done <<< "$files"
 
 
