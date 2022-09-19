7fd4bc82e8c211a3f8a501f351d570a301e4b685
c_ringwald <42188996+datalogism@users.noreply.github.com>
2022-09-19 19:03:50 +0200

    Update clean_multilang.sh

--- dbpedia-loader/process/clean_multilang.sh
+++ dbpedia-loader/process/clean_multilang.sh
@@ -8,12 +8,7 @@ resp_count=$(run_virtuoso_cmd "SPARQL SELECT COUNT(?S) FROM <http://fr.dbpedia.o
 nb_todelete=$(get_answer_nb "$resp_count");
 while [ $nb_todelete -ne 0 ]
 do
-	$(run_virtuoso_cmd "SPARQL \
-       DEFINE sql:log-enable 2  WITH <http://fr.dbpedia.org/graph/dbpedia_generic_labels> \
-       DELETE { ?S ?p ?o. } \
-       WHERE {   \
-       SELECT ?S ?p ?o FROM <http://fr.dbpedia.org/graph/dbpedia_generic_labels> WHERE { \
-       ?S ?p ?o . FILTER NOT EXISTS { ?S a ?t } }  LIMIT $limit }};");
+       $(run_virtuoso_cmd "SPARQL DEFINE sql:log-enable 2  WITH <http://fr.dbpedia.org/graph/dbpedia_generic_labels> DELETE { ?S ?p ?o. } WHERE { SELECT ?S ?p ?o FROM <http://fr.dbpedia.org/graph/dbpedia_generic_labels> WHERE { ?S ?p ?o . FILTER NOT EXISTS { ?S a ?t } }  LIMIT $limit }};");
        resp_count=$(run_virtuoso_cmd "SPARQL SELECT COUNT(?S) FROM <http://fr.dbpedia.org/graph/dbpedia_generic_labels> WHERE { ?S ?p ?o . FILTER NOT EXISTS { ?S a ?t } };" 
        nb_todelete=$(get_answer_nb "$resp_count");
 done
