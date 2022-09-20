6d1d79dab7acca96717164468010ef3054f2c867
c_ringwald <42188996+datalogism@users.noreply.github.com>
2022-09-20 18:33:58 +0200

    Update clean_multilang.sh

--- dbpedia-loader/process/clean_multilang.sh
+++ dbpedia-loader/process/clean_multilang.sh
@@ -7,7 +7,7 @@ echo "============>>>>>>>>>> DELETE NOT USED LABELS 2";
 resp_count=$(run_virtuoso_cmd "SPARQL SELECT COUNT(?S) FROM <http://fr.dbpedia.org/graph/dbpedia_generic_labels> WHERE { ?S ?p ?o . FILTER NOT EXISTS { ?S a ?t } };"); 
 nb_todelete=$(get_answer_nb "$resp_count");
 echo "nb to delete > : $nb_todelete";
-while [ $nb_todelete -ne 0 ]
+while [ "$nb_todelete" -ne 0 ]
 do
        $(run_virtuoso_cmd "SPARQL DEFINE sql:log-enable 2  WITH <http://fr.dbpedia.org/graph/dbpedia_generic_labels> DELETE { ?S ?p ?o. } WHERE { SELECT ?S ?p ?o FROM <http://fr.dbpedia.org/graph/dbpedia_generic_labels> WHERE { ?S ?p ?o . FILTER NOT EXISTS { ?S a ?t } }  LIMIT $limit }};");
        resp_count=$(run_virtuoso_cmd "SPARQL SELECT COUNT(?S) FROM <http://fr.dbpedia.org/graph/dbpedia_generic_labels> WHERE { ?S ?p ?o . FILTER NOT EXISTS { ?S a ?t } };"); 
