39d4c353eb56d84b5703c5bd7038631ef47e8c80
c_ringwald <42188996+datalogism@users.noreply.github.com>
2022-09-22 08:24:35 +0200

    Update clean_multilangv2.sh

--- dbpedia-loader/process/clean_multilangv2.sh
+++ dbpedia-loader/process/clean_multilangv2.sh
@@ -8,14 +8,15 @@ lang_list=$(echo $resp | tr " " "\n" | grep -oP "lang_\K(.*)");
 echo "============>>>>>>>>>> DELETE NOT USED LABELS BY LANG"; 
 for lang in ${lang_list[@]}; do
 	if [[ $lang != 'fr' ]]; then
-    echo "============>>>>>>>>>> $lang"
-    resp_count=$(run_virtuoso_cmd "SPARQL SELECT COUNT(?S) FROM <http://fr.dbpedia.org/graph/dbpedia_generic_labels> WHERE { ?S ?p ?o . FILTER NOT EXISTS { ?S a ?t }. FILTER(lang(?o) = '$lang') };") 
-    nb_todelete=$(get_answer_nb "$resp_count");
-    while [ "$nb_todelete" -ne 0 ]
-    do
-       echo "TO DELETE : $nb_todelete";
-       resp_delete=$(run_virtuoso_cmd "SPARQL WITH <http://fr.dbpedia.org/graph/dbpedia_generic_labels> DELETE { ?S <http://www.w3.org/2000/01/rdf-schema#label> ?o. }  WHERE { SELECT ?S ?o FROM <http://fr.dbpedia.org/graph/dbpedia_generic_labels> WHERE { ?S <http://www.w3.org/2000/01/rdf-schema#label> ?o . FILTER NOT EXISTS { ?S a ?t }. FILTER(lang(?o) = '$lang') }  LIMIT $limit };")
-       resp_count=$(run_virtuoso_cmd "SPARQL SELECT COUNT(?S) FROM <http://fr.dbpedia.org/graph/dbpedia_generic_labels> WHERE { ?S ?p ?o . FILTER NOT EXISTS { ?S a ?t }. FILTER(lang(?o) = '$lang') };") 
-       nb_todelete=$(get_answer_nb "$resp_count");
-    done
+		echo "============>>>>>>>>>> $lang"
+		resp_count=$(run_virtuoso_cmd "SPARQL SELECT COUNT(?S) FROM <http://fr.dbpedia.org/graph/dbpedia_generic_labels> WHERE { ?S ?p ?o . FILTER NOT EXISTS { ?S a ?t }. FILTER(lang(?o) = '$lang') };") 
+		nb_todelete=$(get_answer_nb "$resp_count");
+		while [ "$nb_todelete" -ne 0 ]
+		do
+			echo "TO DELETE : $nb_todelete";
+			resp_delete=$(run_virtuoso_cmd "SPARQL WITH <http://fr.dbpedia.org/graph/dbpedia_generic_labels> DELETE { ?S <http://www.w3.org/2000/01/rdf-schema#label> ?o. }  WHERE { SELECT ?S ?o FROM <http://fr.dbpedia.org/graph/dbpedia_generic_labels> WHERE { ?S <http://www.w3.org/2000/01/rdf-schema#label> ?o . FILTER NOT EXISTS { ?S a ?t }. FILTER(lang(?o) = '$lang') }  LIMIT $limit };")
+			resp_count=$(run_virtuoso_cmd "SPARQL SELECT COUNT(?S) FROM <http://fr.dbpedia.org/graph/dbpedia_generic_labels> WHERE { ?S ?p ?o . FILTER NOT EXISTS { ?S a ?t }. FILTER(lang(?o) = '$lang') };") 
+			nb_todelete=$(get_answer_nb "$resp_count");
+		done
+	fi
 done
