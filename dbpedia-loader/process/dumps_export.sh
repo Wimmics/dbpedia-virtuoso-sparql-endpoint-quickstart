6055900ba59581a1cb27171aea877e5470ae2b38
c_ringwald <42188996+datalogism@users.noreply.github.com>
2022-09-22 10:33:27 +0200

    Update dumps_export.sh

--- dbpedia-loader/process/dumps_export.sh
+++ dbpedia-loader/process/dumps_export.sh
@@ -2,17 +2,24 @@
 #!/usr/bin/env bash
 . ../virtuoso_fct.sh --source-only
 
-
-value=`cat ../virtuoso_proc/create_dump`
-run_virtuoso_cmd  "$value"
-
-cd $DATA_DIR
+cd ${DATA_DIR}/lastUpdate
 mkdir -p computed_dumps
-#cd ${DATA_DIR}/computed_dumps
+cd ${DATA_DIR}/lastUpdate/computed_dumps
 #echo ">>>>>>> DUMP METADATA"
 #run_virtuoso_cmd "dump_one_graph ('http://fr.dbpedia.org/graph/metadata', '${STORE_DATA_DIR}/computed_dump/metadata_computed_', 1000000000000000);"
-#echo ">>>>>>> DUMP LABELS" 
-#run_virtuoso_cmd "dump_one_graph ('http://fr.dbpedia.org/graph/dbpedia_generic_labels', '${STORE_DATA_DIR}/computed_dumps/labels_computed_', 1000000000000000);"
+echo ">>>>>>> DUMP LABELS" 
+run_virtuoso_cmd "dump_one_graph_no_limits ('http://fr.dbpedia.org/graph/dbpedia_generic_labels', '${STORE_DATA_DIR}/lastUpdate/computed_dumps/labels_corrected');"
+#echo ">>>>>>> DUMP GEOCORRECTED" 
+#run_virtuoso_cmd "dump_one_graph_no_limits ('http://fr.dbpedia.org/graph/dbpedia_generic_labels', '${STORE_DATA_DIR}/computed_dumps/labels_corrected');"
+
+################### SPARQL - GET ALL NAMED WIKIDATA GRAPH
+get_named_graph='SPARQL SELECT ?o FROM <http://fr.dbpedia.org/graph/metadata> WHERE { ?s sd:namedGraph ?o. FILTER( ?o != <http://fr.dbpedia.org/graph/dbpedia_wikidata_sameas-all-wikis> AND ?o != <http://fr.dbpedia.org/graph/dbpedia_generic_interlanguage-links> AND STRSTARTS(STR(?o), "http://fr.dbpedia.org/graph/dbpedia_wikidata_") ) };'
+resp=$(run_virtuoso_cmd "$get_named_graph");
+graph_list=$(echo $resp | tr " " "\n" | grep -E "\/graph\/");
+for graph in ${graph_list[@]}; do
+    echo ">> DUMP $graph"
+    run_virtuoso_cmd "dump_one_graph_no_limits ('$graph', '${STORE_DATA_DIR}/lastUpdate/computed_dumps/${graph}_corrected');"
+done
 #echo ">>>>>>> DUMP WIKIDATA SUBCLASS OF" 
 #run_virtuoso_cmd "dump_one_graph ('http://fr.dbpedia.org/graph/dbpedia_wikidata_mappingbased-literals', '${STORE_DATA_DIR}/computed_dumps/wikidata-subclassof_computed_', 1000000000000000);"
 #echo ">>>>>>> DUMP WIKIDATA GEOCOORD" 
