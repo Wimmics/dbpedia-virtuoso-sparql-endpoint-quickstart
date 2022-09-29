fb942b15d6393f9cba75c13209d84397a20b2299
c_ringwald <42188996+datalogism@users.noreply.github.com>
2022-09-29 10:37:56 +0200

    Update clean_wikidata_step3.sh

--- dbpedia-loader/process/clean_wikidata_step3.sh
+++ dbpedia-loader/process/clean_wikidata_step3.sh
@@ -2,7 +2,7 @@
 
 limit=500000;
 
-echo "============>>>> DELETE USELESS WIKIDATA IN FLAGED GRAPHS3"
+echo "============>>>> DELETE USELESS WIKIDATA IN FLAGED GRAPHS 3"
 nb_todo1=1;
 while [ "$nb_todo1" -ne 0 ]
 do
@@ -20,13 +20,13 @@ echo "----- wikidata_labels"
 nb_todo2=1;
 while [ "$nb_todo2" -ne 0 ]
 do
-	resp_delete1=$(run_virtuoso_cmd "SPARQL DEFINE sql:log-enable 2 \
+	resp_delete2=$(run_virtuoso_cmd "SPARQL DEFINE sql:log-enable 2 \
 	    SPARQL WITH  <http://fr.dbpedia.org/graph/dbpedia_wikidata_labels> \
 	    DELETE {?s ?p2 ?o2.} WHERE {\
 	    ?s ?p2 ?o2. FILTER NOT EXISTS { ?s rdf:type ?t} };");
-	resp_todo1=$(run_virtuoso_cmd "SPARQL DEFINE sql:log-enable 2 \
+	resp_todo2=$(run_virtuoso_cmd "SPARQL DEFINE sql:log-enable 2 \
 	SPARQL SELECT COUNT(?s) FROM  <http://fr.dbpedia.org/graph/dbpedia_wikidata_labels> WHERE {\
 	?s ?p2 ?o2. FILTER NOT EXISTS { ?s rdf:type ?t} };" );
-	nb_todo2=$(get_answer_nb "$resp_todo1");
-	echo "wikidata_labels need to delete object : $nb_todo1";
+	nb_todo2=$(get_answer_nb "$resp_todo2");
+	echo "wikidata_labels need to delete object : $nb_todo2";
 done
