e3637bcef17ec4e08bcd48b8454e91d92e5a6eb1
c_ringwald <42188996+datalogism@users.noreply.github.com>
2022-09-29 14:32:37 +0200

    Update clean_wikidata_step3.sh

--- dbpedia-loader/process/clean_wikidata_step3.sh
+++ dbpedia-loader/process/clean_wikidata_step3.sh
@@ -7,11 +7,11 @@ nb_todo1=1;
 while [ "$nb_todo1" -ne 0 ]
 do
 	resp_delete1=$(run_virtuoso_cmd "SPARQL DEFINE sql:log-enable 2 \
-	    SPARQL WITH  <http://fr.dbpedia.org/graph/dbpedia_wikidata_sameas-all-wikis> \
+	    WITH  <http://fr.dbpedia.org/graph/dbpedia_wikidata_sameas-all-wikis> \
 	    DELETE {?s ?p2 ?o2.} WHERE { \
 	    ?s ?p2 ?o2. FILTER NOT EXISTS { ?s rdf:type ?t} };");
 	resp_todo1=$(run_virtuoso_cmd "SPARQL DEFINE sql:log-enable 2 \
-	SPARQL SELECT COUNT(?s) FROM  <http://fr.dbpedia.org/graph/dbpedia_wikidata_sameas-all-wikis> WHERE { \
+	SELECT COUNT(?s) FROM  <http://fr.dbpedia.org/graph/dbpedia_wikidata_sameas-all-wikis> WHERE { \
 	?s ?p2 ?o2. FILTER NOT EXISTS { ?s rdf:type ?t} };" );
 	nb_todo1=$(get_answer_nb "$resp_todo1");
 	echo "sameas-all-wikis need to delete objects : $nb_todo1";
@@ -21,11 +21,11 @@ nb_todo2=1;
 while [ "$nb_todo2" -ne 0 ]
 do
 	resp_delete2=$(run_virtuoso_cmd "SPARQL DEFINE sql:log-enable 2 \
-	    SPARQL WITH  <http://fr.dbpedia.org/graph/dbpedia_wikidata_labels> \
+	    WITH  <http://fr.dbpedia.org/graph/dbpedia_wikidata_labels> \
 	    DELETE {?s ?p2 ?o2.} WHERE { \
 	    ?s ?p2 ?o2. FILTER NOT EXISTS { ?s rdf:type ?t} };");
 	resp_todo2=$(run_virtuoso_cmd "SPARQL DEFINE sql:log-enable 2 \
-	SPARQL SELECT COUNT(?s) FROM  <http://fr.dbpedia.org/graph/dbpedia_wikidata_labels> WHERE { \
+	SELECT COUNT(?s) FROM  <http://fr.dbpedia.org/graph/dbpedia_wikidata_labels> WHERE { \
 	?s ?p2 ?o2. FILTER NOT EXISTS { ?s rdf:type ?t} };" );
 	nb_todo2=$(get_answer_nb "$resp_todo2");
 	echo "wikidata_labels need to delete object : $nb_todo2";
