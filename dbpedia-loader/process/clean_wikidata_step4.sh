61e5ddf5f6421f73df1cb142bd388774d0cf1666
c_ringwald <42188996+datalogism@users.noreply.github.com>
2022-09-29 14:52:39 +0200

    Update clean_wikidata_step4.sh

--- dbpedia-loader/process/clean_wikidata_step4.sh
+++ dbpedia-loader/process/clean_wikidata_step4.sh
@@ -7,11 +7,11 @@ nb_todo2=1;
 while [ "$nb_todo2" -ne 0 ]
 do
 	resp_delete2=$(run_virtuoso_cmd "SPARQL DEFINE sql:log-enable 2 \
-	    SPARQL WITH <http://fr.dbpedia.org/graph/dbpedia_wikidata_sameas-all-wikis> \
+	    WITH <http://fr.dbpedia.org/graph/dbpedia_wikidata_sameas-all-wikis> \
 	    DELETE { ?s a ?t. } WHERE { \
 	    ?s a ?t };");
 	resp_todo2=$(run_virtuoso_cmd "SPARQL DEFINE sql:log-enable 2 \
-	SPARQL SELECT COUNT(?s) FROM  <http://fr.dbpedia.org/graph/dbpedia_wikidata_sameas-all-wikis> WHERE { \
+	SELECT COUNT(?s) FROM  <http://fr.dbpedia.org/graph/dbpedia_wikidata_sameas-all-wikis> WHERE { \
 	?s a ?t };" );
 	nb_todo2=$(get_answer_nb "$resp_todo2");
 	echo "sameas-all-wikis need to change objects : $nb_todo2";
@@ -21,11 +21,11 @@ nb_todo2=1;
 while [ "$nb_todo2" -ne 0 ]
 do
 	resp_delete2=$(run_virtuoso_cmd "SPARQL DEFINE sql:log-enable 2 \
-	    SPARQL WITH <http://fr.dbpedia.org/graph/dbpedia_wikidata_labels> \
+	     WITH <http://fr.dbpedia.org/graph/dbpedia_wikidata_labels> \
 	    DELETE { ?s a ?t. } WHERE { \
 	    ?s a ?t };");
 	resp_todo2=$(run_virtuoso_cmd "SPARQL DEFINE sql:log-enable 2 \
-	SPARQL SELECT COUNT(?s) FROM  <http://fr.dbpedia.org/graph/dbpedia_wikidata_labels> WHERE { \
+	SELECT COUNT(?s) FROM  <http://fr.dbpedia.org/graph/dbpedia_wikidata_labels> WHERE { \
 	?s a ?t };" );
 	nb_todo2=$(get_answer_nb "$resp_todo2");
 	echo "wikidata_labels need to change objects : $nb_todo2";
