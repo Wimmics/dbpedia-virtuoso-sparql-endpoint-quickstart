5f9d85a1d39cc8d83cc43fea44621d4fd6589f1f
c_ringwald <42188996+datalogism@users.noreply.github.com>
2022-10-25 22:32:35 +0200

    Update clean_wikidata_step1.sh

--- dbpedia-loader/process/clean_wikidata_step1.sh
+++ dbpedia-loader/process/clean_wikidata_step1.sh
@@ -19,7 +19,7 @@ do
      SELECT ?s FROM <http://fr.dbpedia.org/graph/dbpedia_wikidata_labels> \
      WHERE { ?s ?p ?o. FILTER (lang(?o)= 'fr').  FILTER NOT EXISTS { ?s rdf:type dbo:WdtHaveFrLabel }} LIMIT $limit \
     };");
-    resp_count=$(run_virtuoso_cmd "SPARQL SELECT ?s FROM <http://fr.dbpedia.org/graph/dbpedia_wikidata_labels> \
+    resp_count=$(run_virtuoso_cmd "SPARQL SELECT COUNT(?s) FROM <http://fr.dbpedia.org/graph/dbpedia_wikidata_labels> \
      WHERE { ?s ?p ?o. FILTER (lang(?o)= 'fr').  FILTER NOT EXISTS { ?s rdf:type dbo:WdtHaveFrLabel }};)");         
     nb_global=$(get_answer_nb "$resp_count");
 done
