745205e70bab57f5357d889d67d367c747e1b929
c_ringwald <42188996+datalogism@users.noreply.github.com>
2022-10-27 09:44:49 +0200

    Update and rename clean_wikidata_step3.sh to clean_wikidata_step3bis.sh

--- dbpedia-loader/process/clean_wikidata_step3bis.sh
+++ dbpedia-loader/process/clean_wikidata_step3bis.sh
@@ -16,17 +16,4 @@ do
 	nb_todo1=$(get_answer_nb "$resp_todo1");
 	echo "sameas-all-wikis need to delete objects : $nb_todo1";
 done
-echo "----- wikidata_labels"
-nb_todo2=1;
-while [ "$nb_todo2" -ne 0 ]
-do
-	resp_delete2=$(run_virtuoso_cmd "SPARQL DEFINE sql:log-enable 2 \
-	    WITH  <http://fr.dbpedia.org/graph/dbpedia_wikidata_labels> \
-	    DELETE {?s ?p2 ?o2.} WHERE { \
-	    ?s ?p2 ?o2. FILTER NOT EXISTS { ?s rdf:type ?t} };");
-	resp_todo2=$(run_virtuoso_cmd "SPARQL DEFINE sql:log-enable 2 \
-	SELECT COUNT(?s) FROM  <http://fr.dbpedia.org/graph/dbpedia_wikidata_labels> WHERE { \
-	?s ?p2 ?o2. FILTER NOT EXISTS { ?s rdf:type ?t} };" );
-	nb_todo2=$(get_answer_nb "$resp_todo2");
-	echo "wikidata_labels need to delete object : $nb_todo2";
-done
+
