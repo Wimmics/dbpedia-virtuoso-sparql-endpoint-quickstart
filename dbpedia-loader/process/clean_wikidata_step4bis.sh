042f160c378b5f4ff4b599d64a712aa5cc6278a3
c_ringwald <42188996+datalogism@users.noreply.github.com>
2022-10-27 09:45:20 +0200

    Update and rename clean_wikidata_step4.sh to clean_wikidata_step4bis.sh

--- dbpedia-loader/process/clean_wikidata_step4bis.sh
+++ dbpedia-loader/process/clean_wikidata_step4bis.sh
@@ -16,17 +16,4 @@ do
 	nb_todo2=$(get_answer_nb "$resp_todo2");
 	echo "sameas-all-wikis need to change objects : $nb_todo2";
 done
-echo "----- wikidata_labels"
-nb_todo2=1;
-while [ "$nb_todo2" -ne 0 ]
-do
-	resp_delete2=$(run_virtuoso_cmd "SPARQL DEFINE sql:log-enable 2 \
-	     WITH <http://fr.dbpedia.org/graph/dbpedia_wikidata_labels> \
-	    DELETE { ?s a ?t. } WHERE { \
-	    ?s a ?t };");
-	resp_todo2=$(run_virtuoso_cmd "SPARQL DEFINE sql:log-enable 2 \
-	SELECT COUNT(?s) FROM  <http://fr.dbpedia.org/graph/dbpedia_wikidata_labels> WHERE { \
-	?s a ?t };" );
-	nb_todo2=$(get_answer_nb "$resp_todo2");
-	echo "wikidata_labels need to change objects : $nb_todo2";
-done
+
