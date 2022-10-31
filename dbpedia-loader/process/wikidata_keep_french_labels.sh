97cf0cc3d424dfd0838a518a5c362c7d4b237574
c_ringwald <42188996+datalogism@users.noreply.github.com>
2022-10-31 12:35:27 +0100

    Update wikidata_keep_french_labels.sh

--- dbpedia-loader/process/wikidata_keep_french_labels.sh
+++ dbpedia-loader/process/wikidata_keep_french_labels.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 ## UNZIP FILE WITH WIKILABELS
-wiki_labels_file_base=($(find  ${STORE_DATA_DIR} -maxdepth 1 -name 'dbpedia_wikidata_labels*'))
+wiki_labels_file_base=($(find  ${DATA_DIR} -maxdepth 1 -name 'dbpedia_wikidata_labels*'))
 gunzip -d ${wiki_labels_file_base}
-wiki_labels_file_2=($(find  ${STORE_DATA_DIR}  find . -maxdepth 1 -name 'dbpedia_wikidata_labels*.ttl' ))
-sudo awk '/@fr/' ${wiki_labels_file_2} >> '${STORE_DATA_DIR}/dbpedia_wikidata_labels_corrected_onlyfrench.ttl'
+wiki_labels_file_2=($(find  ${DATA_DIR}  find . -maxdepth 1 -name 'dbpedia_wikidata_labels*.ttl' ))
+sudo awk '/@fr/' ${wiki_labels_file_2} >> '${DATA_DIR}/dbpedia_wikidata_labels_corrected_onlyfrench.ttl'
 
-mkdir ${STORE_DATA_DIR}/derived_data
-mv ${wiki_labels_file_2} ${STORE_DATA_DIR}/derived_data/
-wiki_labels_file_used=($(find  ${STORE_DATA_DIR}/derived_data/ -maxdepth 1 -name 'dbpedia_wikidata_labels*'))
+mkdir ${DATA_DIR}/derived_data
+mv ${wiki_labels_file_2} ${DATA_DIR}/derived_data/
+wiki_labels_file_used=($(find  ${DATA_DIR}/derived_data/ -maxdepth 1 -name 'dbpedia_wikidata_labels*'))
 sudo gzip  -f ${wiki_labels_file_used}
 ## ZIP FILES
 
-sudo gzip  -f '${STORE_DATA_DIR}/computed_dumps/dbpedia_wikidata_labels_corrected_onlyfrench.ttl'
+sudo gzip  -f '${DATA_DIR}/computed_dumps/dbpedia_wikidata_labels_corrected_onlyfrench.ttl'
