a3fbee0b1ab8ccc94e1cfca56423333f38951c94
c_ringwald <42188996+datalogism@users.noreply.github.com>
2022-11-04 11:41:31 +0100

    Update clean_wikidata_step1_new.sh

--- dbpedia-loader/process/clean_wikidata_step1_new.sh
+++ dbpedia-loader/process/clean_wikidata_step1_new.sh
@@ -21,7 +21,7 @@ do
               SELECT ?s  FROM <http://fr.dbpedia.org/graph/dbpedia_wikidata_sameas-all-wikis> WHERE { \
               ?s rdf:type dbo:WdtFrResource \
               }  \
-       } LIMIT $limit };");
+       } } LIMIT $limit };");
     resp_count=$(run_virtuoso_cmd "SPARQL SELECT COUNT(?s) FROM <http://fr.dbpedia.org/graph/dbpedia_wikidata_sameas-all-wikis> \
      WHERE { ?s rdf:type dbo:WdtHaveFrLabel };");         
     nb_global=$(get_answer_nb "$resp_count");
