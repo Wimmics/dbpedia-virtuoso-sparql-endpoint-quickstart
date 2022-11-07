05196ed36e92f6b6170a7c3558b842a2d6307948
c_ringwald <42188996+datalogism@users.noreply.github.com>
2022-11-07 16:16:53 +0100

    Update clean_wikidata_step1_new.sh

--- dbpedia-loader/process/clean_wikidata_step1_new.sh
+++ dbpedia-loader/process/clean_wikidata_step1_new.sh
@@ -3,25 +3,27 @@
 
 limit=500000;
 
-echo "============>>>> EXPORT TAG TO SAME AS new version"
+echo "============>>>> EXPORT TAG TO SAME AS - Version November"
 count=0;
 nb_global=1;
 last=0;
 while [ "$nb_global" -ne "$last" ]
 do
     echo "NEW LOOP $nb_global not equals to  $last" ;
-    last=$nb_global;	
+    last=$nb_global;  
     resp2=$(run_virtuoso_cmd "SPARQL  \
     DEFINE sql:log-enable 2 \
-    INSERT { GRAPH  <http://fr.dbpedia.org/graph/dbpedia_wikidata_sameas-all-wikis>  {?s rdf:type dbo:WdtHaveFrLabel.} } \
+    PREFIX tag-fr: <http://fr.dbpedia.org/tag/> \
+    PREFIX oa: <http://www.w3.org/ns/oa#> \
+    INSERT { GRAPH  <http://fr.dbpedia.org/graph/dbpedia_wikidata_sameas-all-wikis>  { tag-fr:WdtHaveFrLabel oa:hasTarget ?s.} } \
     WHERE { \
     SELECT ?s FROM <http://fr.dbpedia.org/graph/dbpedia_wikidata_labels> \
         WHERE {  ?s ?p2 ?o2. MINUS {  \
               SELECT ?s  FROM <http://fr.dbpedia.org/graph/dbpedia_wikidata_sameas-all-wikis> WHERE { \
-              ?s rdf:type dbo:WdtFrResource \
+               tag-fr:WdtHaveFrLabel oa:hasTarget ?s \
               }  \
        } } LIMIT $limit };");
     resp_count=$(run_virtuoso_cmd "SPARQL SELECT COUNT(?s) FROM <http://fr.dbpedia.org/graph/dbpedia_wikidata_sameas-all-wikis> \
-     WHERE { ?s rdf:type dbo:WdtHaveFrLabel };");         
+     WHERE {  tag-fr:WdtHaveFrLabel oa:hasTarget ?s };");         
     nb_global=$(get_answer_nb "$resp_count");
 done
