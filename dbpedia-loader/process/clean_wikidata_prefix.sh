96875a421bee72aa1cdefe7484791d35fe8a9b78
c_ringwald <42188996+datalogism@users.noreply.github.com>
2022-11-07 11:23:06 +0100

    Create clean_wikidata_prefix.sh

---
+++ dbpedia-loader/process/clean_wikidata_prefix.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+wikidata_files=`ls ${DATA_DIR}/lastUpdate/dbpedia_wikidata_*`
+for file in $wikidata_files
+do
+   cp $file ${DATA_DIR}/lastUpdate/derived_data/
+   filename=$(basename -- "$file")
+   extension="${filename##*.}"
+   if [[ "$extension" ==  'gz' ]]; then
+      gunzip -d ${file}
+      filename2="${filename%.*}"
+   else
+      filename2=$filename
+   fi
+   sed -i -e 's/dbpedia-wikidata:Q/wikidata:Q/g' ${filename2}
+   sed -i -e 's/<http:\/\/wikidata\.dbpedia\.org\/resource\/Q/wikidata:Q/g' ${filename2}
+   sudo gzip  -f ${filename2}
+     
+done
