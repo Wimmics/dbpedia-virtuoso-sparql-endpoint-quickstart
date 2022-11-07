df14050baf13be3134375ca1391b48f762f8d454
c_ringwald <42188996+datalogism@users.noreply.github.com>
2022-11-07 16:07:31 +0100

    Update multilingual_labels_corrected.sh

--- dbpedia-loader/process/multilingual_labels_corrected.sh
+++ dbpedia-loader/process/multilingual_labels_corrected.sh
@@ -3,7 +3,7 @@
 
 limit=500000;
 
-echo "============ multilingual_labels2.sh V6 "
+echo "============ multilingual_labels.sh Version november 2022"
 ################### SPARQL - GET LANG LIST
 resp=$(run_virtuoso_cmd "SPARQL SELECT DISTINCT CONCAT('lang_',?lang) FROM <http://fr.dbpedia.org/graph/dbpedia_generic_labels> where { ?s rdfs:label ?o. BIND (lang(?o) AS ?lang) };";);
 echo $resp;
@@ -32,8 +32,21 @@ for lang in ${lang_list[@]}; do
 		while [ "$nb_global_wkd" -ne "$last_wkd" ]
 		do
 			last_wkd=$nb_global_wkd;
-			resp_wikilinks_flag=$(run_virtuoso_cmd "SPARQL  WITH <http://fr.dbpedia.org/graph/dbpedia_generic_labels> DELETE { ?s_lang rdfs:label ?o_lang. } INSERT { ?s_fr rdf:type prop-fr:${Lang}FrResource. ?s_fr rdfs:label ?o_lang.} WHERE {SELECT  ?s_fr ?s_lang ?o_lang  FROM <http://fr.dbpedia.org/graph/dbpedia_generic_labels>  WHERE {?s_lang rdfs:label ?o_lang. {SELECT ?s_fr ?s_lang FROM <http://fr.dbpedia.org/graph/dbpedia_wikidata_sameas-all-wikis> WHERE { ?s_fr owl:sameAs ?s_lang. ?s_fr rdf:type  prop-fr:WdtFrResource } } . FILTER NOT EXISTS { ?s_fr rdf:type  prop-fr:${Lang}FrResource } . FILTER(lang(?o_lang)='$lang') } LIMIT $limit };");
-			resp_count=$(run_virtuoso_cmd "SPARQL SELECT COUNT(?s_fr) FROM <http://fr.dbpedia.org/graph/dbpedia_generic_labels> WHERE { ?s_fr rdf:type prop-fr:${Lang}FrResource };");
+			resp_wikilinks_flag=$(run_virtuoso_cmd "SPARQL  WITH <http://fr.dbpedia.org/graph/dbpedia_generic_labels> \
+				DELETE { ?s_lang rdfs:label ?o_lang. } \
+				INSERT { tag-fr:${Lang}FrResource oa:hasTarget ?s_fr. ?s_fr rdfs:label ?o_lang.} \
+				WHERE {\
+					SELECT  ?s_fr ?s_lang ?o_lang  FROM <http://fr.dbpedia.org/graph/dbpedia_generic_labels>  WHERE {\
+					?s_lang rdfs:label ?o_lang.\
+					{ SELECT ?s_fr ?s_lang FROM <http://fr.dbpedia.org/graph/dbpedia_wikidata_sameas-all-wikis> WHERE { \
+						?s_fr owl:sameAs ?s_lang.\
+						 ?s_fr rdf:type  prop-fr:WdtFrResource } } \
+						 . FILTER NOT EXISTS { tag-fr:${Lang}FrResource oa:hasTarget ?s_fr } \
+						 . FILTER(lang(?o_lang)='$lang') \
+			    } LIMIT $limit };");
+
+			resp_count=$(run_virtuoso_cmd "SPARQL SELECT COUNT(?s_fr) FROM <http://fr.dbpedia.org/graph/dbpedia_generic_labels> WHERE {\
+			 tag-fr:${Lang}FrResource oa:hasTarget ?s_fr };");
 			nb_global_wkd=$(get_answer_nb "$resp_count");
 			echo ">>> nb flags WKD : $nb_global_wkd";
 		done
