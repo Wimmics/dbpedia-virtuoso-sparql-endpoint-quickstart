07de454c5a7e48fbac52b4bc6bc8c8fc64923463
c_ringwald <42188996+datalogism@users.noreply.github.com>
2022-11-07 11:56:09 +0100

    Update clean_wikidata_prefix.sh

--- dbpedia-loader/process/clean_wikidata_prefix.sh
+++ dbpedia-loader/process/clean_wikidata_prefix.sh
@@ -3,6 +3,7 @@
 wikidata_files=`ls ${DATA_DIR}/lastUpdate/dbpedia_wikidata_*`
 for file in $wikidata_files
 do
+   echo $file
    cp $file ${DATA_DIR}/lastUpdate/derived_data/
    filename=$(basename -- "$file")
    extension="${filename##*.}"
@@ -13,8 +14,9 @@ do
       filename2=$filename
    fi
    echo $filename2
-   sed -i -e 's/dbpedia-wikidata:Q/wikidata:Q/g' ${filename2}
-   sed -i -e 's/<http:\/\/wikidata\.dbpedia\.org\/resource\/Q/wikidata:Q/g' ${filename2}
+   sed -i -e 's/dbpedia-wikidata:Q/wikidata:Q/g' ${DATA_DIR}/lastUpdate/${filename2}
+   sed -i -e 's/<http:\/\/wikidata\.dbpedia\.org\/resource\/Q/wikidata:Q/g' ${DATA_DIR}/lastUpdate/${filename2}
    gzip  -f ${filename2}
+   echo "end of replacements in file"
      
 done
