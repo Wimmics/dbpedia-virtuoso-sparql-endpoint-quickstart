6950073f14e136bd630770961c9190836b062557
c_ringwald <42188996+datalogism@users.noreply.github.com>
2022-11-07 16:18:25 +0100

    Update clean_multilang_corrected.sh

--- dbpedia-loader/process/clean_multilang_corrected.sh
+++ dbpedia-loader/process/clean_multilang_corrected.sh
@@ -12,17 +12,30 @@ for lang in ${lang_list[@]}; do
 
     Lang="${lang[@]^}"
     echo "============>>>>>>>>>> $lang"
-    resp_count=$(run_virtuoso_cmd "SPARQL SELECT COUNT(?S) FROM <http://fr.dbpedia.org/graph/dbpedia_generic_labels> WHERE { ?S ?p ?o . FILTER NOT EXISTS { tag-fr:${Lang}FrResource oa:hasTarget ?S } };") 
+    
+    resp_count=$(run_virtuoso_cmd "SPARQL PREFIX tag-fr: <http://fr.dbpedia.org/tag/> \
+    PREFIX oa: <http://www.w3.org/ns/oa#> \
+    SELECT COUNT(?S) FROM <http://fr.dbpedia.org/graph/dbpedia_generic_labels> WHERE { \
+    ?S ?p ?o . FILTER NOT EXISTS { tag-fr:${Lang}FrResource oa:hasTarget ?S } };") 
+    
     nb_todelete=$(get_answer_nb "$resp_count");
     while [ "$nb_todelete" -ne 0 ]
     do
       echo "TO DELETE : $nb_todelete";
-      resp_delete=$(run_virtuoso_cmd "SPARQL WITH <http://fr.dbpedia.org/graph/dbpedia_generic_labels>\
+      
+      resp_delete=$(run_virtuoso_cmd "SPARQL WITH <http://fr.dbpedia.org/graph/dbpedia_generic_labels> \
+       PREFIX tag-fr: <http://fr.dbpedia.org/tag/> \
+       PREFIX oa: <http://www.w3.org/ns/oa#> \
        DELETE { ?S <http://www.w3.org/2000/01/rdf-schema#label> ?o. }  WHERE { \
       SELECT ?S ?o FROM <http://fr.dbpedia.org/graph/dbpedia_generic_labels> WHERE {\
       ?S <http://www.w3.org/2000/01/rdf-schema#label> ?o .\
       FILTER NOT EXISTS { tag-fr:${Lang}FrResource oa:hasTarget ?S  } }  LIMIT $limit };")
-      resp_count=$(run_virtuoso_cmd "SPARQL SELECT COUNT(?S) FROM <http://fr.dbpedia.org/graph/dbpedia_generic_labels> WHERE { ?S ?p ?o . FILTER NOT EXISTS { tag-fr:${Lang}FrResource oa:hasTarget ?S };") 
+      
+      resp_count=$(run_virtuoso_cmd "SPARQL PREFIX tag-fr: <http://fr.dbpedia.org/tag/> \
+      PREFIX oa: <http://www.w3.org/ns/oa#> \
+      7SELECT COUNT(?S) FROM <http://fr.dbpedia.org/graph/dbpedia_generic_labels> WHERE { \
+      ?S ?p ?o . FILTER NOT EXISTS { tag-fr:${Lang}FrResource oa:hasTarget ?S };") 
+      
       nb_todelete=$(get_answer_nb "$resp_count");
     done
   fi
