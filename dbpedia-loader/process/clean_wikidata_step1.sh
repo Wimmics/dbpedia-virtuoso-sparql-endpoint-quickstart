d1935bb9650083ac73249dde708cd0517f2137f8
c_ringwald <42188996+datalogism@users.noreply.github.com>
2022-11-07 18:26:35 +0100

    Update and rename clean_wikidata_step2_new.sh to clean_wikidata_step1.sh

--- dbpedia-loader/process/clean_wikidata_step1.sh
+++ dbpedia-loader/process/clean_wikidata_step1.sh
@@ -2,22 +2,20 @@
 
 limit=500000;
 
-echo "============>>>> DELETE USELESS WIKIDATA IN FLAGED GRAPHS 5"
+echo "============>>>> DELETE WIKIDATA WITHOUT TAG"
 nb_todo1=1;
 while [ "$nb_todo1" -ne 0 ]
 do
 	resp_delete1=$(run_virtuoso_cmd "SPARQL DEFINE sql:log-enable 2 \
-	    PREFIX tag-fr: <http://fr.dbpedia.org/tag/> \
             PREFIX oa: <http://www.w3.org/ns/oa#> \
 	    WITH  <http://fr.dbpedia.org/graph/dbpedia_wikidata_sameas-all-wikis> \
 	    DELETE {?s ?p2 ?o2.} WHERE { \
 	    SELECT ?s ?p2 ?o2 FROM  <http://fr.dbpedia.org/graph/dbpedia_wikidata_sameas-all-wikis>  WHERE { \
-	    ?s ?p2 ?o2. FILTER NOT EXISTS {  tag-fr:WdtFrResource oa:hasTarget ?s. } } LIMIT $limit };");
+	    ?s ?p2 ?o2. FILTER NOT EXISTS {  ?t oa:hasTarget ?s. } } LIMIT $limit };");
 	resp_todo1=$(run_virtuoso_cmd "SPARQL \
-	    PREFIX tag-fr: <http://fr.dbpedia.org/tag/> \
             PREFIX oa: <http://www.w3.org/ns/oa#> \
 	SELECT COUNT(?s) FROM  <http://fr.dbpedia.org/graph/dbpedia_wikidata_sameas-all-wikis> WHERE { \
-	?s ?p2 ?o2. FILTER NOT EXISTS { tag-fr:WdtFrResource oa:hasTarget ?s. } };" );
+	?s ?p2 ?o2. FILTER NOT EXISTS { ?t oa:hasTarget ?s. } };" );
 	nb_todo1=$(get_answer_nb "$resp_todo1");
 	echo "sameas-all-wikis need to delete objects : $nb_todo1";
 done
