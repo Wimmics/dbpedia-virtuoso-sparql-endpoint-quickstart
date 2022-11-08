408206663a68a45b8bbb8419bcb332da5c9b2cc0
c_ringwald <42188996+datalogism@users.noreply.github.com>
2022-11-08 19:09:58 +0100

    Update clean_wikidata_prefix.sh

--- dbpedia-loader/process/bash/clean_wikidata_prefix.sh
+++ dbpedia-loader/process/bash/clean_wikidata_prefix.sh
@@ -4,7 +4,7 @@ wikidata_files=`ls ${DATA_DIR}/lastUpdate/dbpedia_wikidata_*`
 for file in $wikidata_files
 do
    echo $file
-   cp --backup=numbered $file ${DATA_DIR}/lastUpdate/derived_data/
+   cp $file ${DATA_DIR}/lastUpdate/derived_data/
    filename=$(basename -- "$file")
    extension="${filename##*.}"
    if [[ "$extension" ==  'gz' ]]; then
