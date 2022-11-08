30fb9ad87a563abcb6cca97c3a6182dc643b7aa1
c_ringwald <42188996+datalogism@users.noreply.github.com>
2022-11-08 17:40:36 +0100

    Update import_conductor.sh

--- dbpedia-loader/import_conductor.sh
+++ dbpedia-loader/import_conductor.sh
@@ -108,7 +108,7 @@ run_virtuoso_cmd "checkpoint_interval(-1)";
 echo ">>>>>>>>>>>>>>>>>>>> CLEAN LANG TAGS"
 /bin/bash ./process/CLEAN_tag_lang.sh
 echo ">>>>>>>>>>>>>>>>>>>> CLEAN WIKIDATA TAGS"
-/bin/bash ./process/CLEAN_tag_wikidata.sh
+#/bin/bash ./process/CLEAN_tag_wikidata.sh
 
 ############## CHANGE GEOLOC COORD FROM TRIPLE TO BLANK NODE
 if [ $PROCESS_GEOLOC == 1 ] ; then
