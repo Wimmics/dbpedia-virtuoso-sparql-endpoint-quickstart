c6df508e314f9f5c9ec8bd5364a5b5e0df0ab13b
c_ringwald <42188996+datalogism@users.noreply.github.com>
2022-11-08 09:45:31 +0100

    Create quick_wikidata_filter.sh

---
+++ dbpedia-loader/process/bash/quick_wikidata_filter.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+wikidata_files=`ls ${DATA_DIR}/lastUpdate/dbpedia_wikidata_*`
+for file in $wikidata_files
+do
+   echo $file
+   cp --backup=numbered $file ${DATA_DIR}/lastUpdate/derived_data/
+   filename=$(basename -- "$file")
+   extension="${filename##*.}"
+   if [[ "$extension" ==  'gz' ]]; then
+      gunzip -d ${file}
+      filename2="${filename%.*}"
+   else
+      filename2=$filename
+   fi
+   echo $filename2
+   awk '!/wikidata:/' $filename2
+   gzip  -f ${filename2}
+   echo "end of replacements in file"
+     
+done
