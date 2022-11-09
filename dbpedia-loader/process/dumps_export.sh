830d9c48b29638de55d29748ab24547049ea9cae
c_ringwald <42188996+datalogism@users.noreply.github.com>
2022-11-09 09:14:02 +0100

    Update dumps_export.sh

--- dbpedia-loader/process/dumps_export.sh
+++ dbpedia-loader/process/dumps_export.sh
@@ -4,13 +4,14 @@
 
 cd ${DATA_DIR}/lastUpdate
 mkdir -p computed_dumps
+today=$(date +'%m_%d_%Y')
 cd ${DATA_DIR}/lastUpdate/computed_dumps
 echo ">>>>>>> DUMP METADATA"
 run_virtuoso_cmd "dump_one_graph_no_limits ('http://fr.dbpedia.org/graph/metadata', '${STORE_DATA_DIR}/lastUpdate/computed_dump/metadata_computed');"
 echo ">>>>>>> DUMP LABELS" 
-run_virtuoso_cmd "dump_one_graph_no_limits ('http://fr.dbpedia.org/graph/dbpedia_generic_labels', '${STORE_DATA_DIR}/lastUpdate/computed_dumps/dbpedia_generic_labels_corrected');"
+run_virtuoso_cmd "dump_one_graph_no_limits ('http://fr.dbpedia.org/graph/dbpedia_generic_labels', '${STORE_DATA_DIR}/lastUpdate/computed_dumps/dbpedia_generic_labels_corrected_${today}');"
 echo ">>>>>>> DUMP GEOCORRECTED" 
-run_virtuoso_cmd "dump_one_graph_no_limits ('http://fr.dbpedia.org/graph/dbpedia_generic_geo-coordinates', '${STORE_DATA_DIR}/computed_dumps/dbpedia_generic_geo-coordinates_corrected');"
+run_virtuoso_cmd "dump_one_graph_no_limits ('http://fr.dbpedia.org/graph/dbpedia_generic_geo-coordinates', '${STORE_DATA_DIR}/computed_dumps/dbpedia_generic_geo-coordinates_corrected_${today}');"
 
 ################### SPARQL - GET ALL NAMED WIKIDATA GRAPH
 get_named_graph='SPARQL SELECT ?o FROM <http://fr.dbpedia.org/graph/metadata> WHERE { ?s sd:namedGraph ?o. FILTER( STRSTARTS(STR(?o), "http://fr.dbpedia.org/graph/dbpedia_wikidata_") ) };'
@@ -19,7 +20,7 @@ graph_list=$(echo $resp | tr " " "\n" | grep -E "\/graph\/");
 for graph in ${graph_list[@]}; do
     fn=$(echo "$graph" | grep -Po '(?<=\/graph\/)(.*)')
     echo ">> DUMP $graph to > $fn"
-    run_virtuoso_cmd "dump_one_graph_no_limits ('$graph', '${STORE_DATA_DIR}/lastUpdate/computed_dumps/${fn}_corrected');"
+    run_virtuoso_cmd "dump_one_graph_no_limits ('$graph', '${STORE_DATA_DIR}/lastUpdate/computed_dumps/${fn}_corrected_${today}');"
 done
 
 
