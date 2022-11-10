d6bde157ec8ab41fb188650ffcb1d248b3ef4d69
c_ringwald <42188996+datalogism@users.noreply.github.com>
2022-11-10 17:52:51 +0100

    Update clean_multilang.sh

--- dbpedia-loader/process/clean_multilang.sh
+++ dbpedia-loader/process/clean_multilang.sh
@@ -2,39 +2,28 @@
 . ../virtuoso_fct.sh --source-only
 
 limit=500000;
-################### SPARQL - GET LANG LIST
-resp=$(run_virtuoso_cmd "SPARQL SELECT DISTINCT CONCAT('lang_',?lang) FROM <http://fr.dbpedia.org/graph/dbpedia_generic_labels> where { ?s rdfs:label ?o. BIND (lang(?o) AS ?lang) };";);
-echo $resp;
-lang_list=$(echo $resp | tr " " "\n" | grep -oP "lang_\K(.*)");
-echo "============>>>>>>>>>> DELETE NOT USED LABELS BY LANG Version november"; 
-for lang in ${lang_list[@]}; do
-  if [[ $lang != 'fr' ]]; then
 
-    Lang="${lang[@]^}"
-    echo "============>>>>>>>>>> $lang"
-    
-    resp_count=$(run_virtuoso_cmd "SPARQL PREFIX oa: <http://www.w3.org/ns/oa#> \
-    SELECT COUNT(?S) FROM <http://fr.dbpedia.org/graph/dbpedia_generic_labels> WHERE { \
-    ?S <http://www.w3.org/2000/01/rdf-schema#label> ?o . FILTER NOT EXISTS { ?t oa:hasTarget ?S }. FILTER(lang(?o) != 'fr') };") 
-    
-    nb_todelete=$(get_answer_nb "$resp_count");
-    while [ "$nb_todelete" -ne 0 ]
-    do
-      echo "TO DELETE : $nb_todelete";
-      
-      resp_delete=$(run_virtuoso_cmd "SPARQL  DEFINE sql:log-enable 2 \
-       PREFIX oa: <http://www.w3.org/ns/oa#> \
-       WITH <http://fr.dbpedia.org/graph/dbpedia_generic_labels> \
-       DELETE { ?S <http://www.w3.org/2000/01/rdf-schema#label> ?o. }  WHERE { \
-      SELECT ?S ?o FROM <http://fr.dbpedia.org/graph/dbpedia_generic_labels> WHERE { \
-      ?S <http://www.w3.org/2000/01/rdf-schema#label> ?o . \
-      FILTER NOT EXISTS { ?t oa:hasTarget ?S  }. FILTER(lang(?o) != 'fr')  }  LIMIT $limit };")
-      
-      resp_count=$(run_virtuoso_cmd "SPARQL PREFIX oa: <http://www.w3.org/ns/oa#> \
-      SELECT COUNT(?S) FROM <http://fr.dbpedia.org/graph/dbpedia_generic_labels> WHERE { \
-      ?S <http://www.w3.org/2000/01/rdf-schema#label> ?o . FILTER NOT EXISTS { ?t oa:hasTarget ?S }. FILTER(lang(?o) != 'fr') };")
-      
-      nb_todelete=$(get_answer_nb "$resp_count");
-    done
-  fi
+
+resp_count=$(run_virtuoso_cmd "SPARQL PREFIX oa: <http://www.w3.org/ns/oa#> \
+SELECT COUNT(?S) FROM <http://fr.dbpedia.org/graph/dbpedia_generic_labels> WHERE { \
+?S <http://www.w3.org/2000/01/rdf-schema#label> ?o . FILTER NOT EXISTS { ?t oa:hasTarget ?S }. FILTER(lang(?o) != 'fr') };") 
+
+nb_todelete=$(get_answer_nb "$resp_count");
+while [ "$nb_todelete" -ne 0 ]
+do
+  echo "TO DELETE : $nb_todelete";
+
+  resp_delete=$(run_virtuoso_cmd "SPARQL  DEFINE sql:log-enable 2 \
+   PREFIX oa: <http://www.w3.org/ns/oa#> \
+   WITH <http://fr.dbpedia.org/graph/dbpedia_generic_labels> \
+   DELETE { ?S <http://www.w3.org/2000/01/rdf-schema#label> ?o. }  WHERE { \
+  SELECT ?S ?o FROM <http://fr.dbpedia.org/graph/dbpedia_generic_labels> WHERE { \
+  ?S <http://www.w3.org/2000/01/rdf-schema#label> ?o . \
+  FILTER NOT EXISTS { ?t oa:hasTarget ?S  }. FILTER(lang(?o) != 'fr')  }  LIMIT $limit };")
+
+  resp_count=$(run_virtuoso_cmd "SPARQL PREFIX oa: <http://www.w3.org/ns/oa#> \
+  SELECT COUNT(?S) FROM <http://fr.dbpedia.org/graph/dbpedia_generic_labels> WHERE { \
+  ?S <http://www.w3.org/2000/01/rdf-schema#label> ?o . FILTER NOT EXISTS { ?t oa:hasTarget ?S }. FILTER(lang(?o) != 'fr') };")
+
+  nb_todelete=$(get_answer_nb "$resp_count");
 done
