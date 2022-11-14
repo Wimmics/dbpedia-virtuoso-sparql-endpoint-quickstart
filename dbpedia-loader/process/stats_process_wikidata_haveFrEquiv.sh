85b67b10fb310960989864bdc08486c89c938343
c_ringwald <42188996+datalogism@users.noreply.github.com>
2022-11-14 18:14:58 +0100

    Update stats_process_wikidata_haveFrEquiv.sh

--- dbpedia-loader/process/stats_process_wikidata_haveFrEquiv.sh
+++ dbpedia-loader/process/stats_process_wikidata_haveFrEquiv.sh
@@ -283,7 +283,7 @@ for graph in ${graph_list_wikidata[@]}; do
     run_virtuoso_cmd "SPARQL  PREFIX void: <http://rdfs.org/ns/void#> \
     PREFIX tag-fr: <http://fr.dbpedia.org/tag/> \
     PREFIX oa: <http://www.w3.org/ns/oa#> \
-    INSERT INTO <${DOMAIN}/graph/statistics> {<$graph> void:classPartition [ void:class tag-fr:WdtFrResource  ; void:classPartition [ void:class ?c ; void:propertyPartition [void:property ?p ; void:distinctSubjects ?x ] ]]. } \
+    INSERT INTO <${DOMAIN}/graph/statistics> { <$graph> void:classPartition [ void:class tag-fr:WdtFrResource  ; void:classPartition [ void:class ?c ; void:propertyPartition [void:property ?p ; void:distinctSubjects ?x ] ]]. } \
     WHERE { SELECT (COUNT(DISTINCT ?s) AS ?x) ?c ?p FROM <$graph>  WHERE {?s ?p ?o. \
      { SELECT ?s FROM  <http://fr.dbpedia.org/graph/dbpedia_wikidata_sameas-all-wikis> \
         WHERE { tag-fr:WdtFrResource oa:hasTarget ?s } } . \
