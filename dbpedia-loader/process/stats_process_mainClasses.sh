ec35ea340b89b3cb5bf2f59cec7e2543e8583802
c_ringwald <42188996+datalogism@users.noreply.github.com>
2022-11-19 00:04:08 +0100

    Create stats_process_mainClasses.sh

---
+++ dbpedia-loader/process/stats_process_mainClasses.sh
@@ -0,0 +1,181 @@
+#!/usr/bin/env bash
+. ../virtuoso_fct.sh --source-only
+
+echo "######################### PART CLASS AND PROP FOR FIRST LEVEL CLASS"
+
+declare -a first_level_class=("dbo:Person" "dbo:Organisation" "dbo:Place" "dbo:Work" "dbo:Event")
+for class in ${first_level_class[@]}; do
+
+	echo "###################### LITT PROP"
+
+	echo "## FR all"
+	run_virtuoso_cmd "SPARQL PREFIX void: <http://rdfs.org/ns/void#> WITH <${DOMAIN}/graph/statistics> \
+		DELETE { <http://fr.dbpedia.org/abstract_graph/mainclass_prop-obj_dbpediafr> void:classPartition ?bn1. \
+			?bn1 void:class ${class}. \
+			?bn1 void:propertyPartition ?bn2. \
+			?bn2 void:property ?p. \
+			?bn2 void:triples ?x. } \
+		WHERE { \
+			<http://fr.dbpedia.org/abstract_graph/mainclass_prop-obj_dbpediafr> void:classPartition ?bn1. \
+			?bn1 void:class ${class}. \
+			?bn1 void:propertyPartition ?bn2. \
+			?bn2 void:property ?p. \
+			?bn2 void:triples ?x. \
+	 		FILTER (isBlank(?bn1) AND isBlank(?bn2)) \
+	 	};"
+
+	run_virtuoso_cmd "SPARQL PREFIX void: <http://rdfs.org/ns/void#> \
+		INSERT INTO <${DOMAIN}/graph/statistics> { \
+			<http://fr.dbpedia.org/abstract_graph/mainclass_prop-obj_dbpediafr> void:classPartition [ \
+				void:class ${class}; \
+				void:propertyPartition [ \
+					void:property ?p2; \
+					void:triples ?nbtriple \
+				] \
+			] \
+	    } WHERE { \
+	    	SELECT ?p2 count(?s) as ?nbtriple FROM <http://fr.dbpedia.org/graph/dbpedia_generic_page> WHERE { \
+				?s ?p ?o. \
+				FILTER EXISTS { \
+					SELECT ?s  FROM <http://fr.dbpedia.org/graph/dbpedia_mappings_instance-types>  WHERE { \
+						?s a ${class} \
+					} \
+				}.{ \
+		            SELECT ?s ?p2 ?o2 FROM  <http://fr.dbpedia.org/graph/dbpedia_mappings_mappingbased-literals> { \
+			        	?s ?p2 ?o2 \
+			    	} \
+		        } \
+			} GROUP BY ?p2 \
+		};"
+
+	echo "## FR only"
+	run_virtuoso_cmd "SPARQL PREFIX void: <http://rdfs.org/ns/void#> WITH <${DOMAIN}/graph/statistics> \
+		DELETE { <http://fr.dbpedia.org/abstract_graph/mainclass_prop-obj_dbpediafr_only> void:classPartition ?bn1. \
+			?bn1 void:class ${class}. \
+			?bn1 void:propertyPartition ?bn2. \
+			?bn2 void:property ?p. \
+			?bn2 void:triples ?x. } \
+		WHERE { \
+			<http://fr.dbpedia.org/abstract_graph/mainclass_prop-obj_dbpediafr_only> void:classPartition ?bn1. \
+			?bn1 void:class ${class}. \
+			?bn1 void:propertyPartition ?bn2. \
+			?bn2 void:property ?p. \
+			?bn2 void:triples ?x. \
+	 		FILTER (isBlank(?bn1) AND isBlank(?bn2)) \
+	 	};"
+
+	run_virtuoso_cmd "SPARQL PREFIX void: <http://rdfs.org/ns/void#> \
+		INSERT INTO <${DOMAIN}/graph/statistics> { \
+			<http://fr.dbpedia.org/abstract_graph/mainclass_prop-obj_dbpediafr_only> void:classPartition [ \
+				void:class ${class}; \
+				void:propertyPartition [ \
+					void:property ?p2; \
+					void:triples ?nbtriple \
+				] \
+			] \
+	    } WHERE { \
+	    	SELECT ?p2 count(?s) as ?nbtriple FROM <http://fr.dbpedia.org/graph/dbpedia_generic_page> WHERE { \
+				?s ?p ?o. \
+				FILTER EXISTS { \
+					SELECT ?s  FROM <http://fr.dbpedia.org/graph/dbpedia_mappings_instance-types>  WHERE { \
+						?s a ${class} \
+					} \
+				}.{ \
+		            SELECT ?s ?p2 ?o2 FROM  <http://fr.dbpedia.org/graph/dbpedia_mappings_mappingbased-literals> { \
+			        	?s ?p2 ?o2 \
+			    	} \
+				
+		        }. FILTER EXISTS { \
+		            SELECT ?s ?p2 ?o3 FROM  <http://fr.dbpedia.org/graph/dbpedia_wikidata_mappingbased-literals> { \
+			        	?s ?p2 ?o3 \
+			      	} \
+		        } \
+			} GROUP BY ?p2 \
+		};"
+
+	echo "###################### OBJ PROP"
+	echo "## FR all"
+	run_virtuoso_cmd "SPARQL PREFIX void: <http://rdfs.org/ns/void#> WITH <${DOMAIN}/graph/statistics> \
+		DELETE { <http://fr.dbpedia.org/abstract_graph/mainclass_prop-obj_dbpediafr> void:classPartition ?bn1. \
+			?bn1 void:class ${class}. \
+			?bn1 void:propertyPartition ?bn2. \
+			?bn2 void:property ?p. \
+			?bn2 void:triples ?x. } \
+		WHERE { \
+			<http://fr.dbpedia.org/abstract_graph/mainclass_prop-obj_dbpediafr> void:classPartition ?bn1. \
+			?bn1 void:class ${class}. \
+			?bn1 void:propertyPartition ?bn2. \
+			?bn2 void:property ?p. \
+			?bn2 void:triples ?x. \
+	 		FILTER (isBlank(?bn1) AND isBlank(?bn2)) \
+	 	};"
+
+	run_virtuoso_cmd "SPARQL PREFIX void: <http://rdfs.org/ns/void#> \
+		INSERT INTO <${DOMAIN}/graph/statistics> { \
+			<http://fr.dbpedia.org/abstract_graph/mainclass_prop-obj_dbpediafr> void:classPartition [ \
+				void:class ${class}; \
+				void:propertyPartition [ \
+					void:property ?p2; \
+					void:triples ?nbtriple \
+				] \
+			]. \
+	    } WHERE { \
+	    	SELECT ?p2 count(?s) as ?nbtriple FROM <http://fr.dbpedia.org/graph/dbpedia_generic_page> WHERE { \
+				?s ?p ?o. \
+				FILTER EXISTS { \
+					SELECT ?s  FROM <http://fr.dbpedia.org/graph/dbpedia_mappings_instance-types>  WHERE { \
+						?s a ${class} \
+					} \
+				}.{ \
+		            SELECT ?s ?p2 ?o2 FROM  <http://fr.dbpedia.org/graph/dbpedia_mappings_mappingbased-objects-uncleaned> { \
+			        	?s ?p2 ?o2 \
+			    	} \
+		        } \
+			} GROUP BY ?p2 \
+		};"
+
+	echo "## FR only"
+	run_virtuoso_cmd "SPARQL PREFIX void: <http://rdfs.org/ns/void#> WITH <${DOMAIN}/graph/statistics> \
+		DELETE { <http://fr.dbpedia.org/abstract_graph/mainclass_prop-obj_dbpediafr_only> void:classPartition ?bn1. \
+			?bn1 void:class ${class}. \
+			?bn1 void:propertyPartition ?bn2. \
+			?bn2 void:property ?p. \
+			?bn2 void:triples ?x. } \
+		WHERE { \
+			<http://fr.dbpedia.org/abstract_graph/mainclass_prop-obj_dbpediafr_only> void:classPartition ?bn1. \
+			?bn1 void:class ${class}. \
+			?bn1 void:propertyPartition ?bn2. \
+			?bn2 void:property ?p. \
+			?bn2 void:triples ?x. \
+	 		FILTER (isBlank(?bn1) AND isBlank(?bn2)) \
+	 	};"
+
+	run_virtuoso_cmd "SPARQL PREFIX void: <http://rdfs.org/ns/void#> \
+		INSERT INTO <${DOMAIN}/graph/statistics> { \
+			<http://fr.dbpedia.org/abstract_graph/mainclass_prop-obj_dbpediafr_only> void:classPartition [ \
+				void:class ${class}; \
+				void:propertyPartition [ \
+					void:property ?p2; \
+					void:triples ?nbtriple \
+				] \
+			]. \
+	    } WHERE { \
+	    	SELECT ?p2 count(?s) as ?nbtriple FROM <http://fr.dbpedia.org/graph/dbpedia_generic_page> WHERE { \
+				?s ?p ?o. \
+				FILTER EXISTS { \
+					SELECT ?s  FROM <http://fr.dbpedia.org/graph/dbpedia_mappings_instance-types>  WHERE { \
+						?s a ${class} \
+					} \
+				}.{ \
+		            SELECT ?s ?p2 ?o2 FROM  <http://fr.dbpedia.org/graph/dbpedia_mappings_mappingbased-objects-uncleaned> { \
+			        	?s ?p2 ?o2 \
+			    	} \
+				
+		        }. FILTER EXISTS { \
+		            SELECT ?s ?p2 ?o3 FROM  <http://fr.dbpedia.org/graph/dbpedia_wikidata_mappingbased-literals> { \
+			        	?s ?p2 ?o3 \
+			      	} \
+		        } \
+			} GROUP BY ?p2 \
+		};"
+done
