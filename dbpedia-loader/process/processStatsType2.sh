01141e9cfc8814220f7777b9f6391ea5ca79402e
c_ringwald <42188996+datalogism@users.noreply.github.com>
2022-11-18 17:03:57 +0100

    Update and rename processStatsTypeLevel.sh to processStatsType2.sh

--- dbpedia-loader/process/processStatsType2.sh
+++ dbpedia-loader/process/processStatsType2.sh
@@ -1,57 +1,5 @@
 #!/usr/bin/env bash
 . ../virtuoso_fct.sh --source-only
-
-echo "################## LEVEL DISTRIB";
-echo "## DBPEDIA";
-run_virtuoso_cmd "SPARQL PREFIX void: <http://rdfs.org/ns/void#> WITH <${DOMAIN}/graph/statistics> \
-DELETE { <http://fr.dbpedia.org/abstract_graph/type_dbpediafr> prop-fr:levelPartition ?bn. \
-?bn prop-fr:level ?level. ?bn void:entities ?count. } \
-WHERE { <http://fr.dbpedia.org/abstract_graph/type_dbpediafr> prop-fr:levelPartition ?bn. \
-?bn prop-fr:level ?level. \
-?bn void:entities ?count. \
-FILTER (isBlank(?bn)) };"
-
-
-run_virtuoso_cmd "SPARQL PREFIX void: <http://rdfs.org/ns/void#> WITH <${DOMAIN}/graph/statistics> \
-	INSERT INTO <${DOMAIN}/graph/statistics> { \
-		<http://fr.dbpedia.org/abstract_graph/type_dbpediafr> prop-fr:levelPartition [ prop-fr:level ?depth; void:entities ?count ]. \
-	} WHERE { \
-		SELECT ?depth COUNT(?s) as ?count  FROM <http://fr.dbpedia.org/graph/dbpedia_mappings_instance-types> WHERE { \
-			?s a ?class. \
-           { \
-               select ?class (count(?mid)-1 as ?depth) FROM <http://fr.dbpedia.org/graph/dbpedia_ontology> WHERE { \
-  				?class rdfs:subClassOf* ?mid. \
-  				?mid rdfs:subClassOf* owl:Thing \
-				} \
-            } \
-		} group by  ?depth \
-	};"
-
-echo "## WIKIDATA";
-run_virtuoso_cmd "SPARQL PREFIX void: <http://rdfs.org/ns/void#> WITH <${DOMAIN}/graph/statistics> \
-DELETE { <http://fr.dbpedia.org/abstract_graph/type_wikidata> prop-fr:levelPartition ?bn. \
-?bn prop-fr:level ?level. ?bn void:entities ?count. } \
-WHERE { <http://fr.dbpedia.org/abstract_graph/type_wikidata> prop-fr:levelPartition ?bn. \
-?bn prop-fr:level ?level. \
-?bn void:entities ?count. \
-FILTER (isBlank(?bn)) };"
-
-
-run_virtuoso_cmd "SPARQL PREFIX void: <http://rdfs.org/ns/void#> WITH <${DOMAIN}/graph/statistics> \
-	INSERT INTO <${DOMAIN}/graph/statistics> { \
-		<http://fr.dbpedia.org/abstract_graph/type_wikidata> prop-fr:levelPartition [ prop-fr:level ?depth; void:entities ?count ]. \
-	} WHERE { \
-		SELECT ?depth COUNT(?s) as ?count  FROM <http://fr.dbpedia.org/graph/dbpedia_wikidata_instance-types> WHERE { \
-			?s a ?class. \
-           { \
-               select ?class (count(?mid)-1 as ?depth) FROM <http://fr.dbpedia.org/graph/dbpedia_ontology> WHERE { \
-  				?class rdfs:subClassOf* ?mid. \
-  				?mid rdfs:subClassOf* owl:Thing \
-				} \
-            } \
-		} group by  ?depth \
-	};"
-
 echo "################## STATS GENERALES";
 
 echo "################### SPARQL - GLOBAL STATS - Nb distincts classes";
