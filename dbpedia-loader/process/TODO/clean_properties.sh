1d189c031ac5c9c0cd3145a40bdcc0b151c47ca0
c_ringwald <42188996+datalogism@users.noreply.github.com>
2022-11-29 14:47:15 +0100

    Update clean_properties.sh

--- dbpedia-loader/process/TODO/clean_properties.sh
+++ dbpedia-loader/process/TODO/clean_properties.sh
@@ -5,3 +5,11 @@
 
 ################### SPARQL - GLOBAL STATS - Nb entities total
 run_virtuoso_cmd "SPARQL PREFIX void: <http://rdfs.org/ns/void#> INSERT INTO <${DOMAIN}/graph/metadata> { <${DOMAIN}> void:entities ?no . } WHERE { SELECT COUNT(distinct ?s) AS ?no { ?s a [] } };"
+
+
+#### DELETE EMPTY STRINGS
+select COUNT(?s) where {
+ ?s ?p ?o.
+FILTER (datatype(?o) = xsd:string).
+FILTER(?o="")
+}
